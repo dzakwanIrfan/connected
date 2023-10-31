3d21655a60758e0adfdfec049aae2366189fa55b
buriane <brian.purnama@mhs.unsoed.ac.id>
2023-10-31 17:31:27 +0700

    menambahkan tabel history_projects

---
+++ app/Http/Controllers/HistoryProjectController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HistoryProject;
+use App\Http\Requests\StoreHistoryProjectRequest;
+use App\Http\Requests\UpdateHistoryProjectRequest;
+
+class HistoryProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHistoryProjectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HistoryProject $historyProject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HistoryProject $historyProject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHistoryProjectRequest $request, HistoryProject $historyProject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HistoryProject $historyProject)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_10_31_102606_create_history_projects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('history_projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_project');
+            $table->string('deskripsi_project');
+            $table->date('mulai');
+            $table->date('selesai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('history_projects');
+    }
+};
