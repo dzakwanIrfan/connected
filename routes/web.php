66f4c9aa221f786146ade10fbb68c57109dd8b1b
dzakwanIrfan <lexdani368@gamil.com>
2023-11-02 09:25:04 +0700

    menghapus routes di web

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\RegisterController;
+// use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserTaskController;
 
 /*
