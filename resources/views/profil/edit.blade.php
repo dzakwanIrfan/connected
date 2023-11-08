43892c0ccebffbf278cb356582ea6f74f2c763f0
dzakwanIrfan <lexdani368@gamil.com>
2023-11-08 19:12:05 +0700

    menambahkan css edit profil

--- resources/views/profil/edit.blade.php
+++ resources/views/profil/edit.blade.php
@@ -1,38 +1,47 @@
-@extends('workbench.layouts.main')
+@extends('profil.layouts.main')
 
 @section('container')
-    <div class="col col-lg-12 mt-5"><h1>Halo, {{ $user->name }}</h1></div>
-    <div class="row justify-content-center mt-3">
-        <div class="col col-2">
-            <img src="https://source.unsplash.com/500x500?profile" class="img-fluid img-thumbnail">
-        </div>
-        <div class="col col-10">
-            <form action="/users/{{ $user->id }}" method="post">
-                @method('put')
-                @csrf
-                <table class="table table-sm">
-                    <tr>
-                        <td>Nama</td>
-                        <td>:</td>
-                        <td><input type="text" name="name" value="{{ old('name', $user->name) }}"></td>
-                    </tr>
-                    <tr>
-                        <td>Email</td>
-                        <td>:</td>
-                        <td><input type="email" name="email" value="{{ old('email', $user->email) }}"></td>
-                    </tr>
-                    <tr>
-                        <td>Password</td>
-                        <td>:</td>
-                        <td><input type="password" name="password" value="" placeholder="New password"></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td><button type="submit" class="btn btn-primary">Edit</button></td>
-                    </tr>
-                </table>
-            </form>
+    <div class="container">
+        <h1 class="page-title">Edit <b>{{ $user->name }}</b> Profile</h1>
+        <hr>
+        @can('staff')    
+            <a href="/users/{{ $user->id }}" class="button-back">Kembali</a>
+        @endcan
+        @can('owner')
+            <a href="{{ url()->previous() }}" class="button-back">Kembali</a>
+        @endcan
+        <div class="sub-container">
+            <div class="container-foto">
+                <img src="https://source.unsplash.com/500x500?profile" class="profile-image">
+            </div>
+            <div class="container-profil">
+                <form action="/users/{{ $user->id }}" method="post">
+                    @method('put')
+                    @csrf
+                    <table class="profile-table">
+                        <tr>
+                            <td>Nama</td>
+                            <td>:</td>
+                            <td><input type="text" name="name" value="{{ old('name', $user->name) }}"  class="input"></td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td>:</td>
+                            <td><input type="text" name="email" value="{{ old('email', $user->email) }}" class="input"></td>
+                        </tr>
+                        <tr>
+                            <td>Password</td>
+                            <td>:</td>
+                            <td><input type="password" name="password" value="" placeholder="New password" class="input"></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td><button type="submit" class="edit">Edit</button></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
