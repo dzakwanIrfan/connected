2a09f5f39862ba540520d92807eabcebe958a128
dzakwanIrfan <lexdani368@gamil.com>
2023-11-08 17:11:40 +0700

    edit profil.index

--- resources/views/profil/index.blade.php
+++ resources/views/profil/index.blade.php
@@ -1,33 +1,35 @@
-@extends('workbench.layouts.main')
+@extends('profil.layouts.main')
 
 @section('container')
-    <div class="col col-lg-12 mt-5"><h1>Halo, {{ $user->name }}</h1></div>
-    @can('staff')    
-        <a href="/workbench" class="btn btn-dark text-decoration-none mt-2">Kembali</a>
-    @endcan
-    @can('owner')
-        <a href="{{ url()->previous() }}" class="btn btn-dark text-decoration-none mt-2">Kembali</a>
-    @endcan
-    <div class="row justify-content-center mt-3">
-        <div class="col col-2">
-            <img src="https://source.unsplash.com/500x500?profile" class="img-fluid img-thumbnail">
-            @can('edit-profile', $user->id)
-                <a href="/users/{{ $user->id }}/edit" class="badge bg-primary text-decoration-none mt-2">edit</a>
+    <div class="container">
+        <div class="sub-title"><h1>{{ $user->name }} Profile</h1></div><hr>
+            @can('staff')    
+                <a href="/workbench" class="button-back">Kembali</a>
             @endcan
-        </div>
-        <div class="col col-10">
-            <table class="table table-sm">
-                <tr>
-                    <td>Nama</td>
-                    <td>:</td>
-                    <td>{{ $user->name }}</td>
-                </tr>
-                <tr>
-                    <td>Email</td>
-                    <td>:</td>
-                    <td>{{ $user->email }}</td>
-                </tr>
-            </table>
+            @can('owner')
+                <a href="{{ url()->previous() }}" class="button-back">Kembali</a>
+            @endcan
+        <div class="sub-container">
+            <div class="container-foto">
+                <img src="https://source.unsplash.com/500x500?profile" class="img-fluid img-thumbnail">
+                @can('edit-profile', $user->id)
+                    <a href="/users/{{ $user->id }}/edit" style="display: block">edit</a>
+                @endcan
+            </div>
+            <div class="container-profil">
+                <table class="table table-sm">
+                    <tr>
+                        <td>Nama</td>
+                        <td>:</td>
+                        <td>{{ $user->name }}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>:</td>
+                        <td>{{ $user->email }}</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
