7769b4ae5e3dd2b9a684a236a488e812c9c759ec
katsuku27 <148852467+katsuku27@users.noreply.github.com>
2023-11-28 16:54:00 +0700

    menambah css card dan sedikit sidebar

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,19 +6,19 @@
             <div class="card-top">
               <div class="card-edit">
                 <a href="/projects/{{ $project->id }}/edit" class="edit">
-                  <ion-icon name="create-outline" class="card-icon"></ion-icon>
+                  Edit
                 </a>
               </div>
               <div class="card-delete">
                 <form action="/projects/{{ $project->id }}" method="post" class="delete-form" enctype="multipart/form-data">
                   @method('delete')
                   @csrf
-                  <button class="delete-button" onclick="return confirm('Are you sure?');"><ion-icon name="close-circle-outline" class="card-icon"></ion-icon></button>
+                  <button class="delete-button" onclick="return confirm('Are you sure?');">Delete</button>
                 </form>
               </div>
             </div>
               <div class="card-img">
-                @if ($project->image)
+              @if ($project->image)
               <img src="{{ asset('storage/' . $project->image) }}" alt="foto_project" class="img-card"> 
               @else
               <img src="https://source.unsplash.com/1200x400?{{ $project->nama_project }}" alt="" class="img-card">
@@ -27,6 +27,8 @@
               <div class="card-body">
                 <h5 class="card-title"><a href="/projects/{{ $project->id }}/tasks">{{ $project->nama_project }}</a></h5> 
                 <p class="card-text">{{ $project->deskripsi_project }}</p>
+                <p class="card-date">Dimulai dari: <br>{{ $project->mulai }} <br></p>
+                <p class="card-date">Selesai: <br> {{ $project->selesai }} <br></p>
               </div>
             </div>
       @endforeach
