7195bd4d58e477da840c502b8de2e0308ad4429e
Brian <brian.purnama@mhs.unsoed.ac.id>
2023-11-29 00:25:36 +0700

    menambahkan frontend untuk tambah pekerja dan tambah file

--- resources/views/userTasks/create.blade.php
+++ resources/views/userTasks/create.blade.php
@@ -1,11 +1,9 @@
 @extends('usertasks.layouts.main')
 
 @section('container')
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+<div class="tengah">
         <h1>Tambah pekerja untuk task {{ $task->nama_task }}</h1>
-    </div>   
-    
-    <div class="col-lg-8">
+        <hr>
         <form action="/user-task" method="post">
             @csrf
             <input type="hidden" name="task_id" value="{{ $task->id }}">
