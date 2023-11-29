d7c2e007add466eb6ce04d02937a341fb15cb538
katsuku27 <148852467+katsuku27@users.noreply.github.com>
2023-11-29 07:46:12 +0700

    fix overflow projectlist sidebar

--- resources/views/workbench/index.blade.php
+++ resources/views/workbench/index.blade.php
@@ -3,22 +3,22 @@
 @section('container')
   <form action="/logout" method="post">
     @csrf
-    <button type="submit" class="nav-link d-flex align-items-center gap-2 text-black"><i class="bi bi-box-arrow-right"></i> Logout</button>
+    <button type="submit" class="">Logout</button>
   </form>
-  <div class="btn btn-dark">
-    <a href="/users/{{ $user->id }}" class="text-decoration-none text-white">{{ $user->name }}</a>
+  <div class="btn ">
+    <a href="/users/{{ $user->id }}" class="">{{ $user->name }}</a>
   </div>
-  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <div class="">
   <h1 class="h2">Welcome back, {{ auth()->user()->name }}</h1>
   </div>
-  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <div class="col">
   @foreach ($projects as $project)
-  <div class="col-md-4 mb-3">
+  <div class="">
       <div class="card">
         @if ($project->image)
-        <img src="{{ asset('storage/' . $project->image) }}" alt="foto_project" class="img-fluid" style="width: 1200px; height: 150px; object-fit: cover;"> 
+        <img src="{{ asset('storage/' . $project->image) }}" alt="foto_project" class="img-card"> 
         @else
-        <img src="https://source.unsplash.com/1200x400?{{ $project->nama_project }}" alt="" class="img-fluid">
+        <img src="https://source.unsplash.com/1200x400?{{ $project->nama_project }}" alt="" class="img-card">
         @endif
           <div class="card-body">
             <h5 class="card-title"><a href="/projects/{{ $project->id }}/tasks" class="text-decoration-none text-dark">{{ $project->nama_project }}</a></h5> 
