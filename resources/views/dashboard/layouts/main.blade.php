333ea89e6088864882231aedcd59a8808f7fd5b0
Brian <brian.purnama@mhs.unsoed.ac.id>
2023-11-29 21:12:37 +0700

    ada yang typo tadi

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -4,7 +4,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Connected | Workspcae</title>
+    <title>Connected | Workspace</title>
     <link rel="icon" href="/img/connected.png">
     <link rel="stylesheet" href="/css/sidebar.css">
     <link rel="stylesheet" href="/css/card.css">
